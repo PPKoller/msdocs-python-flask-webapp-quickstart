d2b65816afa1d69466529f6ebddaac5dd5b4773f
Patrick Koller <pkoller@thermoplan.ch>
2024-11-22 07:26:57 +0000

    input number

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def favicon():
 
 @app.route("/hello", methods=["POST"])
 def hello():
-    name = float(sqrt(float(request.form.get("name"))))
+    name = request.form.get("name")
 
     if isinstance(name, float) :
         print("Request for hello page received with name=%s" % name)
